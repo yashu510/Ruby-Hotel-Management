3e248cc136add4ced485967a4cf96aa9a0f05d5b
Guna Yaswanth Gadde <92032651+yashu510@users.noreply.github.com>
2024-08-27 20:27:40 -0700

    Update hotel.rb

--- hotel.rb
+++ hotel.rb
@@ -6,31 +6,30 @@ class Hotel
     number_of_rooms.times { @rooms << Room.new }
   end
 
-  def no_vacancies?
+  def no_rooms?
     @rooms.all? { |room| room.party != nil }
   end
 
-  def available_room
+  def avail_room
     @rooms.find { |room| room.party.nil? }
   end
 
-  def list_parties_without_rooms
+  def get_parties_without_rooms
     @rooms.select { |room| room.party.nil? }
   end
 
-  def list_rooms_without_parties
+  def get_rooms_with_parties
     @rooms.reject { |room| room.party.nil? }
   end
 
-  # Updated method to list rooms with assigned party names
-  def list_rooms_with_parties
+  # Updated method to list rooms with Allocated party names
+  def get_partyDetails_from_rooms
     @rooms.each_with_index do |room, index|
       if room.party
         party_guests = room.party.guests.map(&:name).join(", ")
-       # puts "Room #{index + 1}  Party Name: #{room.party_name} has the following  Guests - #{party_guests}"
-        puts "Room #{index + 1}  has #{room.party_name}.  #{room.party_name} have the following  Guests - #{party_guests}"
+        puts "Room #{index + 1}  Has #{room.party_name}.  #{room.party_name} Has the following Guests - #{party_guests}"
       else
-        puts "Room #{index + 1}: No party assigned"
+        puts "Room #{index + 1}: is Available"
         end
     end
   end
