053b949d005edad4d0946af210cb41d11787cbf2
Guna Yaswanth Gadde <92032651+yashu510@users.noreply.github.com>
2024-09-09 22:51:32 -0700

    Update hotel.rb

--- hotel.rb
+++ hotel.rb
@@ -1,10 +1,71 @@
 class Hotel
   attr_accessor :rooms
 
-  def initialize(number_of_rooms)
-    @rooms = []
-    number_of_rooms.times { @rooms << Room.new }
+  def initialize(num_of_rooms)
+    @rooms = Array.new(num_of_rooms) { |i| Room.new(i + 1) }
+    @unassigned_parties = []
   end
+  
+
+  # Check-in a party to the first available room or add to unassigned list
+  def check_in(party)
+    available_room = @rooms.find { |room| !room.occupied? }
+
+    if avail_room
+      avail_room.assign_party(party)
+      puts "Checked in party of #{party.size} to room #{avail_room.number}."
+    else
+      @unassigned_parties << party
+      puts "No available rooms. Party of #{party.size} added to the waiting list."
+    end
+  end
+
+    # Check-out a party from a specific room
+    def check_out(room_number)
+      room = @rooms.find { |r| r.number == room_number }
+  
+      if room
+        room.vacate
+        puts "Party from room #{room_number} is now on the waiting list."
+        @unassigned_parties << room.party if room.party
+      else
+        puts "Room #{room_number} does not exist."
+      end
+    end
+
+    # List the status of all rooms
+  def list_rooms
+    @rooms.each do |room|
+      if room.occupied?
+        party_names = room.party.guests.map(&:name).join(", ")
+        puts "Room #{room.number}: Occupied by party with guests: #{party_names}"
+      else
+        puts "Room #{room.number}: Vacant"
+      end
+    end
+  end
+  
+# Add a new room to the hotel
+def add_room
+  new_room_number = @rooms.size + 1
+  new_room = Room.new(new_room_number)
+  @rooms << new_room
+  puts "Added room #{new_room_number}."
+end
+
+# Remove a room from the hotel, if it's vacant
+def remove_room(room_number)
+  room = @rooms.find { |r| r.number == room_number }
+
+  if room.nil?
+    puts "Room #{room_number} does not exist."
+  elsif room.occupied?
+    puts "Cannot remove room #{room_number} because it is currently occupied."
+  else
+    @rooms.delete(room)
+    puts "Removed room #{room_number}."
+  end
+end
 
   def no_rooms?
     @rooms.all? { |room| room.party != nil }
